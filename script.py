41224c45bafa134c8eac2f5423aaa7aaede0261c
Benedikt Mix <mixbened@gmail.com>
2019-08-07 17:28:29 +0200

    turn contacts into dicts

--- script.py
+++ script.py
@@ -15,10 +15,14 @@ while True:
         print("These are your contacts: \n")
         # loops through list and prints name
         for contact in contacts:
-            print(contact)
+            print(str(contact['index']) + ": " + contact["First Name"] + " " + contact["Last Name"])
     elif action == "add":
-        new_contact = input("Type in new Contact: ")
-        # adds name to list
+        # create dictionary for new contact
+        new_contact = {}
+        new_contact["index"] = len(contacts) + 1
+        new_contact["First Name"] = input("enter first name: ")
+        new_contact["Last Name"] = input("enter last name: ")
+        # adds dictionary to list
         contacts.append(new_contact)
     # not list or add
     else:
